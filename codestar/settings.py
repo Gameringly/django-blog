fd355a0731ad240b2381e92ac366d2cc2caeb49e
Gameringly <luke.x.x.williams@gmail.com>
2025-11-20 17:40:53 +0000

    update code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@nzmr6r0-vi5sj0epvfk_93cpxtrwx%=^#wx0z*s^eb6+ceot-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
