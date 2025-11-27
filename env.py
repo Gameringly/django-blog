491887ad2c6f62679cd0bcf1b63c4d8eb20896a9
Gameringly <luke.x.x.williams@gmail.com>
2025-11-27 12:38:07 +0000

    add secret key

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_4tDdxsJnON8F@ep-floral-credit-aggnga7b.c-2.eu-central-1.aws.neon.tech/grit_claw_fog_554840")
+os.environ.setdefault("SECRET_KEY", "plm098okn765uhb432ygv")
